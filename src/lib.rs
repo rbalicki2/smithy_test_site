0ada0a28d75265e023c042eb90aa285eea15cc91
Robert Balicki <robert.balicki@gmail.com>
2018-12-05 22:47:11 -0600

    get a component with an event handler to compile

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,14 @@ use web_sys::{
 
 use smithy::{
   smd,
-  types::Component,
+  types::{
+    Component,
+    SmithyComponent,
+  },
 };
+
+use wasm_bindgen::JsValue;
+use web_sys::console::log_1;
 
 fn get_window() -> Window {
   unsafe { transmute::<Object, Window>(global()) }
@@ -26,25 +32,38 @@ fn get_document() -> Document {
   get_window().document().unwrap()
 }
 
+struct AppState {
+  click_count: u32,
+}
+
+fn foo<'a, 'b>(count: u32, mut update_count: impl FnMut(u32) -> () + 'a) -> SmithyComponent<'b>
+where
+  'a: 'b,
+{
+  smd!(<h1 on_click={|_| update_count(4)}>{ format!("click = {}", count) }</h1>)
+}
+
 #[wasm_bindgen]
 pub fn start(div_id: String) {
   let doc: Document = get_document();
   let root_element: Element = doc.get_element_by_id(&div_id).unwrap();
 
-  struct AppState {
-    click_count: u32,
-  }
-
   let mut app_state = AppState { click_count: 0 };
 
-  let app = smd!(
-    <h1 on_click={|_| app_state.click_count = app_state.click_count + 1}>
-      hello kerry
-    </h1>
-    <div>
-      I have clicked { format!("{}", app_state.click_count) } times
-    </div>
-  );
+  // TODO log app_state, see if it's changing...
 
-  smithy::mount(app, root_element);
+  let app_2 = smd!(<div>
+    {format!("{}", app_state.click_count) }
+    {
+      &mut foo(
+        app_state.click_count,
+        |new_count| {
+          log_1(&JsValue::from(format!("{}", app_state.click_count)));
+          app_state.click_count = new_count;
+        }
+      )
+    }
+  </div>);
+
+  smithy::mount(Box::new(app_2), root_element);
 }
